cd0b5e4e8c89217b4f4eb0036ec8265cbd28fb9b
AaronMullan <aaroncmullan@gmail.com>
2019-12-16 17:46:03 -0800

    tests are passing, let us break some stuff

--- __tests__/trips.test.js
+++ __tests__/trips.test.js
@@ -16,6 +16,8 @@ describe('trip routes', () => {
     return mongoose.connection.dropDatabase();
   });
   let trip;
+  let itineraries;
+  
   beforeEach(async() => {
     trip = await Trip.create({
       name: 'hawaii'
@@ -36,6 +38,36 @@ describe('trip routes', () => {
           _id: expect.any(String),
           name: 'new york',
           __v:0
+        });
+      });
+  });
+  it('gets a list of trips', async() => {
+    const trips = await Trip.create([
+      { name: 'paris' },
+      { name: 'tokyo' }
+    ]);
+    return request(app)
+    
+      .get('/api/v1/trips')
+      .then(res => {
+        trips.forEach(trip => {
+          expect(res.body).toContainEqual({
+            _id: trip._id.toString(),
+            name: trip.name,
+            __v:0
+          });
+        });
+      });
+  });
+  it('gets a trip by id', async() => {
+    return request(app)
+      .get(`/api/v1/trips/${trip._id}`)
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          name: trip.name,
+          __v:0
+
         });
       });
   });
